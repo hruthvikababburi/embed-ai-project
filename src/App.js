fef81ed174ac3c00e64638829e215e670cbcb1f1
Hruthvika Babburi <hruthvikababburi@gmail.com>
2024-04-13 00:57:55 +0530

    Added Footer.

--- src/App.js
+++ src/App.js
@@ -4,22 +4,26 @@ import Navbar from './Components/Navbar';
 import HomePage from './Components/Homepage';
 import ChatPage from './Components/ChatPage';
 import CreateCharacterPage from './Components/CreateCharacterPage';
+import Footer from './Components/Footer';
 
 const App = () => {
     return (
         <Router>
-            <div className="flex">
-                
-                <Navbar />
+            <div className='flex flex-col h-screen'>
+                <div className="flex flex-grow">
+                    
+                    <Navbar />
 
-                
-                <div className="flex-grow">
-                    <Routes>
-                        <Route exact path="/" element={<HomePage/>} />
-                        <Route path="/chat/:id" element={<ChatPage/>} />
-                        <Route path="/createcharacter" element={<CreateCharacterPage/>} />
-                    </Routes>
+                    
+                    <div className="flex-grow">
+                        <Routes>
+                            <Route exact path="/" element={<HomePage/>} />
+                            <Route path="/chat/:id" element={<ChatPage/>} />
+                            <Route path="/createcharacter" element={<CreateCharacterPage/>} />
+                        </Routes>
+                    </div>
                 </div>
+                <Footer/>
             </div>
         </Router>
     );
